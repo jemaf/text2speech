32664ae5f86def10a0ce480ed4a14d3cb668956b
Joao Eduardo Montandon <edu.montandon@gmail.com>
2013-04-24 16:50:25 -0300

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'thin'
+
+group :assets do
+  
+  gem "less-rails"
+
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+
+  gem 'twitter-bootstrap-rails'
+end
+
+group :development do
+  gem "pry"
+  gem "pry-nav"
+  gem "awesome_print"
+end
+
+group :test do
+  gem "rspec-rails"
+  gem "capybara"
+  gem "factory_girl"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Text2speech::Application.config.secret_token = '8ba98c5ee75e718c2f3c43734e91ad851017cbb773938cdcde99d463f374037cdb3ede0fa9bb6a4bb647c16084315d91a3921388887880a89511578b1f738069'
